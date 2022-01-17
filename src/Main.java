c51962ced593d5ef71fd465e07b3e353aad912f6
kmuller86 <89707079+kmuller86@users.noreply.github.com>
2022-01-17 17:12:31 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,22 +8,33 @@ public class Main {
 
         /* ćw_6 Listy */
 
-        /* zad.1 a) Napisać program który stworzy listę ArrayList<String> (lista tablicowa),
-                Proszę dane wprowadzać z klawiatury (można użyć pętli), lista ma mieć przynajmniej 5
-                elementów i elementami mają być nazwy zwierząt.
+        /* zad.1 Napisać program który 
+                a) stworzy listę ArrayList<String> (lista tablicowa),
+                  Proszę dane wprowadzać z klawiatury (można użyć pętli), lista ma mieć przynajmniej 5
+                  elementów i elementami mają być nazwy zwierząt (niech niektóre wartości dublują się).
+                  -- podpowiedź: List<String> nazwaListy = new ArrayList<>();
                 b) proszę stworzyć metodę która będzie miała pętle forEach i będzie wyświetlała elementy Listy
-                -- podpowiedź: for(String zmienna : List<String>)
-                c) proszę usunąć 2 ostatnie elementy listy i dodać do niej 3 inne elementy,
-                następnie wyświetlić listę i wielkość listy,
-                d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
-                elementów listy
+                  -- podpowiedź: for(String zmienna : List<String>) { }
+                c) proszę usunąć 2 ostatnie elementy listy i drugi element, następnie dodać do niej 4 inne elementy,
+                  następnie wyświetlić listę i wielkość listy,
+                d) proszę wyświetlić posortowną listę, oraz odwrotniee posortowaną i znów użyć metody z punktu b) do wyświetlenia
+                  elementów listy
           */
 
 
-         /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
-                niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
-                 a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
+         /* zad.2 Napisać program który :
+                a) stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury, liczby mają się powtarzać,
+                b) następnie wyświetlić zawartość set-a w konsoli,
+                c) następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
+        
+        
+        /* zad.3* Dla chętnych, nie będzie oceniane:
+                Napisać podobny program jak w zad.1 zamiast zwierząt wprowadzć dowolne dane 
+                np. imiona, nazwiska, miasta, samochody ... . (dowolność),
+                Wykonać na TreeSet 
+                -- podpowiedź: Set<String> nazwaSeta = new TreeSet<>();
+        /*
 
     }
 
