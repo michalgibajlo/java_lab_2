0fed81f257ced2182dbe38df4967c4c1fc9ddda7
michalgibajlo <michal.gibajlo@gmail.com>
2022-01-31 00:31:44 +0100

    Update .gitignore

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
           Jeśli liczba będzie podzielna przez 2 ostatnie cyfry waszego indeksu (podać nr indesu z uczelni)
           to dać komunikat "liczba XX podzielna przez YY" (XX-liczba podzielna YY-podzielnik)
         */
-
+/*
         int x=1000;
         do
         {
@@ -16,14 +16,44 @@ public class Main {
             System.out.println(x + czyparzyste);
         }
         while (--x > 0);
-
+*/
 
         /* zad 2
         a) stworzyć tablicę typu int wielkosci 5, przypisac jej dowolne wartości,
           przeiterować tablicę po elementach i wyswietlić, użyć pętli while
-        b) jak wyżej tylko dla tablicy typu double[],
-        c) jak wyżej dla tablicy String[], przeiterować i wyświetlić od ostatniego elementu
         */
+
+        int[] tab1 = new int[5];
+        tab1[0] = 15; tab1[1] = 45; tab1[2]=13; tab1[3]=78; tab1[4]=29;
+        int i=0;
+        while (i<tab1.length)
+                {
+                    System.out.print(tab1[i]+" ");
+                    i++;
+                }
+        //b) jak wyżej tylko dla tablicy typu double[],
+        System.out.println();
+
+        Double[] tab2 = new Double[5];
+        tab2[0] = 15.0; tab2[1] = 45.0; tab2[2]=13.0; tab2[3]=78.0; tab2[4]=29.0;
+        i=0;
+        while (i<tab2.length)
+            {
+                System.out.print(tab2[i] + " ");
+                i++;
+            }
+
+        //c) jak wyżej dla tablicy String[], przeiterować i wyświetlić od ostatniego elementu
+        System.out.println();
+
+        String[] tab3 = new String[5];
+        tab3[0] = "Ala"; tab3[1] = "ma"; tab3[2]= "kota"; tab3[3]= "i"; tab3[4]= "psa.";
+        i=tab3.length-1;
+        while (i>-1)
+        {
+            System.out.print(tab3[i] + " ");
+            i--;
+        }
 
         /* zad.3
         a) sprawdzić jak zachowuje się pętla while gdy w jej warunku damy true --> while(true)
