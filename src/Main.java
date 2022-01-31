d9a2b4e1109de467c73dfe8b54de8e3762b623c6
michalgibajlo <michal.gibajlo@gmail.com>
2022-01-31 19:16:23 +0100

    Update .gitignore

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,35 @@ public class Main {
         b) w drugiej pętli continue
         c) w trzeciej pętli return
         */
+        int i;
+        System.out.println("\nBREAK on 5");
+        for (i=1; i<=10; i++)
+        {
+            System.out.print(i+" ");
+            if (i==5)
+            {
+                break;
+            }
+        }
 
+        System.out.println("\nCONTINUE on 5");
+        for (i=1; i<=10; i++)
+        {
+            System.out.print(i+" ");
+            if (i==5)
+            {
+                continue;
+            }
+        }
 
+        System.out.println("\nRETURN on 5");
+        for (i=1; i<=10; i++)
+        {
+            System.out.print(i+" ");
+            if (i==5)
+            {
+                return; //metoda void nic nie zwraca
+            }
+        }
     }
 }
