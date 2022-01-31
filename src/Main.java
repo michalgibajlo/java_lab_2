3c48e52901ddb61a6ab94e2517f0ce0942b33958
michalgibajlo <michal.gibajlo@gmail.com>
2022-01-31 01:04:39 +0100

    Update .gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
@@ -8,11 +9,32 @@ public class Main {
          *  i wyświetlał liczby które są podzielne przez ostatnią cyfrę waszego indeksu
          * (jesli tą liczbą jest 0 to przez 2 ostatnie) */
 
+        for (int i=100; i>0; i--)
+        {
+            if (i%5==0)
+            {
+                System.out.print(i+" ");
+            }
+        }
+
 
         /* zad.2 Napisz program który będzie uzupełniał tablicę 5-cio elementową int[] liczbami
          * wprowadzanymi z klawiatury (użyć klasy Scanner), a następnie będzie wyświetlał elementy tablicy
          * za pomocą pętli forEach powiększone o 11 */
+        System.out.println("\n");
 
+        int[] liczby = new int[5];
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj 5 liczb (oddzielonych enterem):");
+        for (int i=0; i<5; i++)
+        {
+            int x = scan.nextInt();
+            liczby[i]=x;
+        }
 
+        for (int x : liczby)
+        {
+            System.out.print(x+11+" ");
+        }
     }
 }
