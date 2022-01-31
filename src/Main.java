e364c87377d77844223a8fe097941a0c607978bb
michalgibajlo <michal.gibajlo@gmail.com>
2022-01-31 17:39:39 +0100

    Update .gitignore

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,26 @@ public class Main {
           b) program który określi czy liczba jest podzielna przez 7
          */
 
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj liczbę: ");
+        int x = scan.nextInt();
+        switch (x%2)
+        {
+            case 0:
+                System.out.println("Parzysta");
+                break;
+            default:
+                System.out.println("Nieparzysta");
+        }
+        switch (x%7)
+        {
+            case 0:
+                System.out.println("Podzielna przez 7");
+                break;
+            default:
+                System.out.println("Niepodzielna przez 7");
+        }
+
         /* zad.2
            a) napisać program podobny do programu z przykładu, który ma zliczyć iloś liter 'o', 'p', 'x', 'z', 'q' w wyrazie:
             "ooooozjoonoxoooozoooooooooozoxooooopnpppoooiiooooomnzooooxoqoojoozoootyoooozoooxoooozooooxooooooojoooopxoooonnmooo"
